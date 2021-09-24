4f13b2d59accbf36072f657b99a712fc9eea91de
wangyibo <2375847849@qq.com>
2021-09-24 22:37:08 +0800

    Update UserDaoTest.java

--- Mybatis-Study/mybatis-01/src/test/java/com/wang/dao/UserDaoTest.java
+++ Mybatis-Study/mybatis-01/src/test/java/com/wang/dao/UserDaoTest.java
@@ -9,17 +9,58 @@ import java.util.List;
 
 public class UserDaoTest {
     @Test
-    public void test() {
+    public void getUserList() {
         // 获得sqlSession对象
         SqlSession sqlSession = MybatisUtils.getSqlSession();
         //方式一：getMapper
-        UserDao mapper = sqlSession.getMapper(UserDao.class);
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
         List<User> userList = mapper.getUserList();
 
         for (User user : userList) {
             System.out.println(user);
         }
         // 关闭流
+        sqlSession.close();
+    }
+    @Test
+    public void getUserById() {
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User userById = mapper.getUserById(1);
+        System.out.println(userById);
+        sqlSession.close();
+    }
+    @Test
+    public void addUser() {
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        mapper.addUser(new User(4,"haha","123123"));
+
+        sqlSession.commit();
+
+        sqlSession.close();
+    }
+    @Test
+    public void updateUser() {
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        mapper.updateUser(new User(4,"方腊","444444"));
+
+        sqlSession.commit();
+
+
+        sqlSession.close();
+    }
+
+    @Test
+    public void deleteUser() {
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        mapper.deleteUser(4);
+
+        sqlSession.commit();
+
+
         sqlSession.close();
     }
 }
