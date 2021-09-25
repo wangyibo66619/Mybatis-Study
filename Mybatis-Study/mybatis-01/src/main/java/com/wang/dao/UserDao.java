97029a3853c1e579ab641c46c69ade4717ed8c06
wangyibo <2375847849@qq.com>
2021-09-25 19:13:21 +0800

    Update UserDao.java

--- Mybatis-Study/mybatis-01/src/main/java/com/wang/dao/UserDao.java
+++ Mybatis-Study/mybatis-01/src/main/java/com/wang/dao/UserDao.java
@@ -3,16 +3,20 @@ package com.wang.dao;
 import com.wang.pojo.User;
 
 import java.util.List;
+import java.util.Map;
 
 public interface UserMapper {
     // 查询全部用户
     List<User> getUserList();
     // 根据ID查询用户
     User getUserById(int id);
+
     // 添加用户
     int addUser(User user);
      // 修改用户
     int updateUser(User user);
     // 删除用户
     int deleteUser(int a);
+    // 模糊查询
+    List<User> getUserList1(String str);
 }
